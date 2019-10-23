f6cdca049dd51580a8ca9e047bddef30d3421ff5
Barbara Anissin <anissinb@gmail.com>
2019-10-23 16:10:07 +0200

    boom

--- Advanced/airplane_exercise_files/myscript.js
+++ Advanced/airplane_exercise_files/myscript.js
@@ -1,17 +1,38 @@
 var fly = true
+const sound = $("#audio")[0]
+
 
 function setfalse() {
     fly = false
+    sound.play()
+    click()
+    $("img[src='airplane.png']").attr("src", "crash.png")
 }
 
-
 function showCoords(event) {
-    var x = event.pageX;
-    var y = event.pageY;
-    console.log(x + " " + y)
     if (fly == true) {
-        $("#moveme").animate({ "top": y - 256 / 2 + "px", "left": x - 256 / 2 + "px" }, 'slow', 'linear')
-    } else {
-        $("#moveme").animate({ "bottom": 0, "top": 500 + "px" }, 'slow', 'linear').fadeOut(500).fadeIn(500)
+        $("#moveme").animate({ "top": event.pageY - 256 / 2, "left": event.pageX - 256 / 2 }, 0)
+        //$("#moveme").offset({ "top": event.pageY - 256 / 2, "left": event.pageX - 256 / 2 })
     }
+}
+
+function click() {
+    $("#moveme").animate({ "bottom": 0, "top": 500 + "px" }, 'slow', 'linear').fadeOut(500).fadeIn(500);
+
+}
+
+let rockets = document.getElementsByClassName("rocket")
+console.log(rockets)
+window.onload = function shoot() {
+    for (i = 0; i < 9; i++) {
+        x = Math.floor(Math.random() * 1500)
+        y = Math.floor(Math.random() * 1500)
+        $(rockets[i]).css({ "position": "absolute", "left": x + "px" }).animate({ "top": 0, "left": y + "px" }, 1000)
+    }
+}
+
+for (i = 9; i < 18; i++) {
+    x = Math.floor(Math.random() * 500)
+    y = Math.floor(Math.random() * 500)
+    $(rockets[i]).css({ "position": "absolute", "left": x + "px" }).animate({ "top": 0, "left": y + "px" }, 1000)
 }
--- Intermediate/myscript.js
+++ Intermediate/myscript.js
@@ -2,11 +2,30 @@ let shit = document.getElementsByTagName("span")
 for (i = 1; i < shit.length; i = i + 4) {
     if (shit[i].innerText == "PC") {
         $(shit[i].parentElement.parentElement.parentElement.parentElement.parentElement.firstElementChild.firstElementChild).css("background-color", "red")
-    }
-    else if (shit[i].innerText == "Mobilephone") {
+    } else if (shit[i].innerText == "Mobilephone") {
         $(shit[i].parentElement.parentElement.parentElement.parentElement.parentElement.firstElementChild.firstElementChild).css("background-color", "green")
-    }
-    else {
+    } else {
         $(shit[i].parentElement.parentElement.parentElement.parentElement.parentElement.firstElementChild.firstElementChild).css("background-color", "blue")
+    }
+}
+
+let filter = document.getElementsByTagName("span")
+
+var val=""
+document.getElementById("opt1").onclick = function(){
+	val = $("#opt1 :selected").text()
+	select()
+}
+
+function select() {
+    for (i = 1; i < filter.length; i = i + 4) {
+        if (filter[i].innerText == val) {
+        	console.log(val)
+            $(filter[i].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement).show()
+        } else if (val == "all"){
+        	$(filter[i].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement).show()
+        }	else {
+        	$(filter[i].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement).hide()
+        } 
     }
 }
